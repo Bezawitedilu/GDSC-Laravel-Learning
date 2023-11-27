cc8934bd23251f08bfa4467b301c1921d4648be7
Bezawit <bezawitedilu@gmail.com>
2023-11-27 22:09:50 +0300

    Created form for publishing post

---
+++ Week_2/Blog Website/posts/publish_post.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: ../auth/login.php');
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $author = $_SESSION['user_id'];
+    $timestamp = date('Y-m-d H:i:s');
+
+    $posts = json_decode(file_get_contents('../data/posts.json'), true);
+
+    $newPost = array(
+        'title' => $title,
+        'content' => $content,
+        'author' => $author,
+        'timestamp' => $timestamp
+    );
+
+    $posts[] = $newPost;
+
+    file_put_contents('../data/posts.json', json_encode($posts));
+
+    header('Location: index.php');
+    exit();
+}
+
+include('../header.php');
+?>
+
+<h2>Publish Post</h2>
+<!-- Form for publishing post -->
+
+<?php include('../footer.php'); ?>
