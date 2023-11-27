1a69fec3f7ed3b5e7c7e413285f73452d317c093
Bezawit <bezawitedilu@gmail.com>
2023-11-27 22:14:34 +0300

    Index page, front page taht has login and singup

---
+++ Week_2/Blog Website/index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: auth/login.php');
+    exit();
+}
+
+include('header.php');
+?>
+
+<h1>Welcome to Bezawit's Blog</h1>
+<!-- Display blog posts from JSON file -->
+
+<?php include('footer.php'); ?>
