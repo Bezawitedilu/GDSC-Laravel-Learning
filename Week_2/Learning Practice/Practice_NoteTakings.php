81c6731f5eb31871c4ce3c166197d5ea1052afa8
Bezawit <bezawitedilu@gmail.com>
2023-11-27 18:45:43 +0300

    Created files

---
+++ Week_2/Learning Practice/Practice_NoteTakings.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    //slide 5
+    <!-- <?php
+    $message = "Hello";
+    echo "Hello message " . $message;
+    //it does not matter to use concatenation or not
+    ?> -->
+
+    
+</body>
+</html>
