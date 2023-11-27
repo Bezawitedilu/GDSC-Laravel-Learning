223d37a18739603897a0e637ae126c396796b6e6
Bezawit <bezawitedilu@gmail.com>
2023-11-28 01:00:42 +0300

    edited home page

--- Week_2/Blog_Website/index.php
+++ Week_2/Blog_Website/index.php
@@ -1,15 +1,24 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Bezawit's Blog</title>
+    <link rel="stylesheet" href="path/to/your/styles.css">
+</head>
+<body class="bg-gray-100">
 
-if (!isset($_SESSION['user_id'])) {
-    header('Location: auth/login.php');
-    exit();
-}
+<div class="container mx-auto mt-8 p-8 bg-white rounded-lg shadow-md max-w-md">
+    <h2 class="text-2xl font-bold mb-6">Welcome to Bezawit's Blog</h2>
 
-include('header.php');
-?>
+    <div class="flex justify-between mb-4">
+        <a href="auth/login.php" class="bg-indigo-600 text-white px-4 py-2 rounded-md">Login</a>
+        <a href="auth/signup.php" class="bg-green-600 text-white px-4 py-2 rounded-md">Signup</a>
+    </div>
 
-<h1>Welcome to Bezawit's Blog</h1>
-<!-- Display blog posts from JSON file -->
+    <!-- You can add additional content here for your welcome page -->
 
-<?php include('footer.php'); ?>
+</div>
+
+</body>
+</html>
