1b80a86460a38c7767c1fc7d8ec0d5745ce77875
Bezawit <bezawitedilu@gmail.com>
2023-11-27 22:25:48 +0300

    publish post edited, can now publish post and identify error

--- Week_2/Blog Website/posts/publish_post.php
+++ Week_2/Blog Website/posts/publish_post.php
@@ -32,7 +32,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 include('../header.php');
 ?>
 
-<h2>Publish Post</h2>
-
+<div class="container mx-auto mt-8 p-8 bg-white rounded-lg shadow-md max-w-md">
+    <h2 class="text-2xl font-bold mb-6">Publish Post</h2>
+    <!-- Form for publishing post -->
+</div>
 
 <?php include('../footer.php'); ?>
