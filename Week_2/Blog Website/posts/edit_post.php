d8fedf1a9dad084790e694038f76c94cbef038d8
Bezawit <bezawitedilu@gmail.com>
2023-11-27 22:24:59 +0300

    Edited edit post to chnage and edit posts

--- Week_2/Blog Website/posts/edit_post.php
+++ Week_2/Blog Website/posts/edit_post.php
@@ -2,14 +2,16 @@
 session_start();
 
 if (!isset($_SESSION['user_id'])) {
-    header('Location: ../login.php');
+    header('Location: ../auth/login.php');
     exit();
 }
 
 include('../header.php');
 ?>
 
-<h2>Edit Post</h2>
+<div class="container mx-auto mt-8 p-8 bg-white rounded-lg shadow-md max-w-md">
+    <h2 class="text-2xl font-bold mb-6">Edit Post</h2>
 
+</div>
 
 <?php include('../footer.php'); ?>
