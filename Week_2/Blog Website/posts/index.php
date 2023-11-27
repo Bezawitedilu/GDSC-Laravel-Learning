755dd69f44b5889befeff500c9d8c9aef6c658ca
Bezawit <bezawitedilu@gmail.com>
2023-11-27 22:24:24 +0300

    Added files to locate

--- Week_2/Blog Website/posts/index.php
+++ Week_2/Blog Website/posts/index.php
@@ -9,7 +9,12 @@ if (!isset($_SESSION['user_id'])) {
 include('../header.php');
 ?>
 
-<h1>Welcome to Bezawit's Blog</h1>
-<!-- Display blog posts from JSON file -->
+<div class="container mx-auto mt-8 p-8 bg-white rounded-lg shadow-md">
+    <h1 class="text-3xl font-bold mb-6">Welcome to Bezawit's Blog</h1>
+
+    <!-- Display blog posts from JSON file -->
+    <!-- You can use PHP to fetch and display posts here -->
+
+</div>
 
 <?php include('../footer.php'); ?>
