9778437586aa7fa97ae5b14f6c30be153d0a7552
Bezawit <bezawitedilu@gmail.com>
2023-11-27 22:15:09 +0300

    Added about page to give more information about websit

---
+++ Week_2/Blog Website/about.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+include('header.php');
+?>
+
+<h1>About Bezawit's Blog</h1>
+<p>This blog is all about sharing interesting stories, insights, and experiences. Stay tuned for more!</p>
+
+<?php include('footer.php'); ?>
