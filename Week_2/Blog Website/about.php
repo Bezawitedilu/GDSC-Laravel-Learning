2ff024fb817c88117a5353429ccfdf6fc29e0105
Bezawit <bezawitedilu@gmail.com>
2023-11-27 22:26:28 +0300

    edited about page and added header and footer

--- Week_2/Blog Website/about.php
+++ Week_2/Blog Website/about.php
@@ -4,7 +4,14 @@ session_start();
 include('header.php');
 ?>
 
-<h1>About Bezawit's Blog</h1>
-<p>This blog is all about sharing interesting stories, insights, and experiences. Stay tuned for more!</p>
+<div class="container mx-auto mt-8 p-8 bg-white rounded-lg shadow-md">
+    <h1 class="text-3xl font-bold mb-6">About Bezawit's Blog</h1>
+
+    <p class="text-lg leading-relaxed">
+        Welcome to Bezawit's Blog, a place where I share my thoughts, experiences, and stories. 
+        Join me on this journey of exploration and discovery.
+    </p>
+
+</div>
 
 <?php include('footer.php'); ?>
